1d630aa529a0aff03838bad28de2792810136e05
VanshKamboj65 <vanshkamboj6573@gmail.com>
2026-08-23 19:42:06 +0530

    Time: 2 ms (51.97%) | Memory: 23.8 MB (60.62%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
 
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 1;
+        int candidate1 = 0;
+        int candidate2 = 1;
 
-        // Find candidates
-        for (int num : nums) {
+        int count1 = 0;
+        int count2 = 0;
 
-            if (num == candidate1) {
+        // Find two possible candidates
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(nums[i] == candidate1) {
                 count1++;
             }
-            else if (num == candidate2) {
+            else if(nums[i] == candidate2) {
                 count2++;
             }
-            else if (count1 == 0) {
-                candidate1 = num;
+            else if(count1 == 0) {
+                candidate1 = nums[i];
                 count1 = 1;
             }
-            else if (count2 == 0) {
-                candidate2 = num;
+            else if(count2 == 0) {
+                candidate2 = nums[i];
                 count2 = 1;
             }
             else {
@@ -28,24 +31,30 @@ public:
             }
         }
 
-        // Verify candidates
+        // Count actual frequencies
         count1 = 0;
         count2 = 0;
 
-        for (int num : nums) {
-            if (num == candidate1)
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(nums[i] == candidate1) {
                 count1++;
-            else if (num == candidate2)
+            }
+
+            if(nums[i] == candidate2) {
                 count2++;
+            }
         }
 
         vector<int> ans;
 
-        if (count1 > nums.size() / 3)
+        if(count1 > nums.size() / 3) {
             ans.push_back(candidate1);
+        }
 
-        if (count2 > nums.size() / 3)
+        if(count2 > nums.size() / 3 && candidate2 != candidate1) {
             ans.push_back(candidate2);
+        }
 
         return ans;
     }
