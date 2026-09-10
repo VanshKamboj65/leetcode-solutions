97e2c2164aa174a5b791d845a80e48e40ef554bd
VanshKamboj65 <vanshkamboj6573@gmail.com>
2026-09-10 18:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (88.04%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        int freq[128]={0};
+        int freq[256]={0};
         for(char ch:s){
             freq[ch]++;
-        }    
+        }
         string ans="";
-       while(true){
-        int index=-1;
-        int maxi=0;
-        for(int i=0;i<128;i++){
-            if(freq[i]>maxi){
-                maxi=freq[i];
-                index=i;
+        while(true){
+            int maxi=0;
+            int index=-1;
+            for(int i=0;i<256;i++){
+                if(freq[i]>maxi){
+                    maxi=freq[i];
+                    index=i;
+                }
             }
+            if(maxi==0) break;
+            while(maxi--){
+                ans.push_back(index);
+            }
+            freq[index]=0;
         }
-        if(maxi==0) break;
-        while(maxi--){
-            ans.push_back(index);
-        }
-        freq[index]=0;
-       }
         return ans;
     }
 };
